5ecaa7eefa497fb6f38f78afab689f9a3f693608
blinge666 <143176600+blinge666@users.noreply.github.com>
2026-03-18 13:34:19 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'festivalplan-v1';
+const CACHE = 'festivalplan-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
